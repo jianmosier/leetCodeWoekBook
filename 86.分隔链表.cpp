0b4b754a68134e946082c49f7fe811a8a6488c28
jianmosier <875816119@qq.com>
2023-04-10 14:47:27 +0000

    first commit

---
+++ 86.分隔链表.cpp
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode.cn id=86 lang=cpp
+ * @lcpr version=21902
+ *
+ * [86] 分隔链表
+ */
+
+// @lc code=start
+/* *
+ * Definition for singly-linked list. */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode* keyPtr;
+        ListNode* ptr;
+        ListNode* lastPtr;
+        lastPtr = head;
+        if(lastPtr->next == nullptr){
+            return lastPtr;
+        }
+        ptr = lastPtr->next;
+        while(ptr->val < x){
+            keyPtr = ptr;
+            lastPtr = ptr; 
+            ptr = ptr->next;
+        }
+        for(; ptr->next != nullptr; lastPtr = ptr, ptr = ptr->next){
+            if(ptr->val < x){
+                ListNode* tempPtr;
+                tempPtr = ptr;
+
+
+                lastPtr->next = ptr->next;
+                tempPtr->next = 
+                keyPtr = keyPtr->next;
+            }
+        }
+
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,4,3,2,5,2]\n3\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [2,1]\n2\n
+// @lcpr case=end
+
+ */
+
+
