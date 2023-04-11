18e12615077761fc099518ac77021514d9431352
jianmosier <875816119@qq.com>
2023-04-11 02:47:30 +0000

    self1

--- 86.分隔链表.cpp
+++ 86.分隔链表.cpp
@@ -8,53 +8,42 @@
 // @lc code=start
 /* *
  * Definition for singly-linked list. */
-/* struct ListNode {
+struct ListNode {
     int val;
     ListNode *next;
     ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
-}; */
+};
 
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        if (head == nullptr || head->next == nullptr) {
+        if(head == nullptr || head->next == nullptr){
             return head;
         }
-
         ListNode dummyHead(-1);
         dummyHead.next = head;
         ListNode* keyPtr = &dummyHead;
         ListNode* lastPtr = &dummyHead;
-        ListNode* ptr = head;
+        ListNode* ptr = dummyHead.next;
 
-        // 寻找第一个大于或等于x的节点，作为插入点
-        while (ptr != nullptr && ptr->val < x) {
-            keyPtr = ptr;
+        while(ptr->val < x){
+            if(ptr->next == nullptr) return dummyHead.next;
             lastPtr = ptr;
+            keyPtr = ptr;
             ptr = ptr->next;
         }
 
-        // 遍历链表，将小于x的节点移动到插入点
-        while (ptr != nullptr) {
-            if (ptr->val < x) {
-                // 删除ptr指向的节点
+        for(; ptr != nullptr; lastPtr = ptr, ptr = ptr->next){
+            if(ptr->val < x ){
                 lastPtr->next = ptr->next;
-
-                // 将ptr指向的节点插入到keyPtr之后
                 ptr->next = keyPtr->next;
                 keyPtr->next = ptr;
 
-                // 更新keyPtr和ptr
-                keyPtr = keyPtr->next;
-                ptr = lastPtr->next;
-            } else {
-                lastPtr = ptr;
-                ptr = ptr->next;
+                ptr = lastPtr;
             }
         }
-
         return dummyHead.next;
     }
 };
